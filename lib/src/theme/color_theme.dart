15b8a27c320d35d675532e0283a1e9725314e644
lehieultt <lehieultt@gmail.com>
2023-07-18 16:00:17 +0700

    Color and font theme

--- lib/src/theme/color_theme.dart
+++ lib/src/theme/color_theme.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+
 class ColorTheme {
-  
+  static Color scienceBlue = const Color(0xff0B4DDD);
+  static Color shark = const Color(0xff20242C);
+  static Color black = const Color(0xff000000);
 }
--- lib/src/theme/font_theme.dart
+++ lib/src/theme/font_theme.dart
@@ -1,3 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:virusscanapp/src/theme/color_theme.dart';
+
 class FontTheme {
-  
+  static TextStyle scienceBlue20W700Inter = GoogleFonts.inter(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w700,
+    color: ColorTheme.scienceBlue,
+  );
+
+
+
+
 }
